d6488e02f6fcd353fd5385101baacbf11177e499
Mario Mol <mariohmol@gmail.com>
2016-03-05 15:07:00 -0300

    first version with all directives declared

--- src/angular-d3plus.js
+++ src/angular-d3plus.js
@@ -2,6 +2,133 @@
 	'use strict';
 
 	angular.module('angular-d3plus', [])
+
+	.directive('d3plusBar',['angularD3plusUtils',function(angularD3plusUtils) {
+	  return {
+	  	restrict: 'AE',
+	  	scope: {
+            data: '=', 
+            id: '=',  
+            container: '@?',    
+            size: '@?'
+        },
+        template:  angularD3plusUtils.template,
+	  	link: angularD3plusUtils.link,
+	  	controller: function($scope, $element){angularD3plusUtils.controller($scope, $element,'bar');}
+	  };
+	}])
+	.directive('d3plusBubbles',['angularD3plusUtils',function(angularD3plusUtils) {
+	  return {
+	  	restrict: 'AE',
+	  	scope: {
+            data: '=', 
+            id: '=',  
+            container: '@?',    
+            size: '@?'
+        },
+        template:  angularD3plusUtils.template,
+	  	link: angularD3plusUtils.link,
+	  	controller: function($scope, $element){angularD3plusUtils.controller($scope, $element,'bubbles');}
+	  };
+	}])
+	.directive('d3plusBox',['angularD3plusUtils',function(angularD3plusUtils) {
+	  return {
+	  	restrict: 'AE',
+	  	scope: {
+            data: '=', 
+            id: '=',  
+            container: '@?',    
+            size: '@?'
+        },
+        template:  angularD3plusUtils.template,
+	  	link: angularD3plusUtils.link,
+	  	controller: function($scope, $element){angularD3plusUtils.controller($scope, $element,'box');}
+	  };
+	}])
+	.directive('d3plusChart',['angularD3plusUtils',function(angularD3plusUtils) {
+	  return {
+	  	restrict: 'AE',
+	  	scope: {
+            data: '=', 
+            id: '=',  
+            container: '@?',    
+            size: '@?'
+        },
+        template:  angularD3plusUtils.template,
+	  	link: angularD3plusUtils.link,
+	  	controller: function($scope, $element){angularD3plusUtils.controller($scope, $element,'chart');}
+	  };
+	}])
+	.directive('d3plusGeomap',['angularD3plusUtils',function(angularD3plusUtils) {
+	  return {
+	  	restrict: 'AE',
+	  	scope: {
+            data: '=', 
+            id: '=',  
+            container: '@?',    
+            size: '@?'
+        },
+        template:  angularD3plusUtils.template,
+	  	link: angularD3plusUtils.link,
+	  	controller: function($scope, $element){angularD3plusUtils.controller($scope, $element,'geo_map');}
+	  };
+	}])
+	.directive('d3plusLine',['angularD3plusUtils',function(angularD3plusUtils) {
+	  return {
+	  	restrict: 'AE',
+	  	scope: {
+            data: '=', 
+            id: '=',  
+            container: '@?',    
+            size: '@?'
+        },
+        template:  angularD3plusUtils.template,
+	  	link: angularD3plusUtils.link,
+	  	controller: function($scope, $element){angularD3plusUtils.controller($scope, $element,'line');}
+	  };
+	}])
+	.directive('d3plusNetwork',['angularD3plusUtils',function(angularD3plusUtils) {
+	  return {
+	  	restrict: 'AE',
+	  	scope: {
+            data: '=', 
+            id: '=',  
+            container: '@?',    
+            size: '@?'
+        },
+        template:  angularD3plusUtils.template,
+	  	link: angularD3plusUtils.link,
+	  	controller: function($scope, $element){angularD3plusUtils.controller($scope, $element,'network');}
+	  };
+	}])
+	.directive('d3plusPaths',['angularD3plusUtils',function(angularD3plusUtils) {
+	  return {
+	  	restrict: 'AE',
+	  	scope: {
+            data: '=', 
+            id: '=',  
+            container: '@?',    
+            size: '@?'
+        },
+        template:  angularD3plusUtils.template,
+	  	link: angularD3plusUtils.link,
+	  	controller: function($scope, $element){angularD3plusUtils.controller($scope, $element,'paths');}
+	  };
+	}])
+	.directive('d3plusPie',['angularD3plusUtils',function(angularD3plusUtils) {
+	  return {
+	  	restrict: 'AE',
+	  	scope: {
+            data: '=', 
+            id: '=',  
+            container: '@?',    
+            size: '@?'
+        },
+        template:  angularD3plusUtils.template,
+	  	link: angularD3plusUtils.link,
+	  	controller: function($scope, $element){angularD3plusUtils.controller($scope, $element,'pie');}
+	  };
+	}])
 	.directive('d3plusRadar',['angularD3plusUtils',function(angularD3plusUtils) {
 		
 	  return {
@@ -17,7 +144,7 @@
 	  	controller: function($scope, $element){angularD3plusUtils.controller($scope, $element,'radar');}
 	  };
 	}])
-	.directive('d3plusTreemap',['angularD3plusUtils',function(angularD3plusUtils) {
+	.directive('d3plusRings',['angularD3plusUtils',function(angularD3plusUtils) {
 	  return {
 	  	restrict: 'AE',
 	  	scope: {
@@ -28,7 +155,35 @@
         },
         template:  angularD3plusUtils.template,
 	  	link: angularD3plusUtils.link,
-	  	controller: function($scope, $element){angularD3plusUtils.controller($scope, $element,'treemap');}
+	  	controller: function($scope, $element){angularD3plusUtils.controller($scope, $element,'rings');}
+	  };
+	}])
+	.directive('d3plusSankey',['angularD3plusUtils',function(angularD3plusUtils) {
+	  return {
+	  	restrict: 'AE',
+	  	scope: {
+            data: '=', 
+            id: '=',  
+            container: '@?',    
+            size: '@?'
+        },
+        template:  angularD3plusUtils.template,
+	  	link: angularD3plusUtils.link,
+	  	controller: function($scope, $element){angularD3plusUtils.controller($scope, $element,'sankey');}
+	  };
+	}])
+	.directive('d3plusScatter',['angularD3plusUtils',function(angularD3plusUtils) {
+	  return {
+	  	restrict: 'AE',
+	  	scope: {
+            data: '=', 
+            id: '=',  
+            container: '@?',    
+            size: '@?'
+        },
+        template:  angularD3plusUtils.template,
+	  	link: angularD3plusUtils.link,
+	  	controller: function($scope, $element){angularD3plusUtils.controller($scope, $element,'scatter');}
 	  };
 	}])
 	.directive('d3plusStacked',['angularD3plusUtils',function(angularD3plusUtils) {
@@ -45,23 +200,55 @@
 	  	controller: function($scope, $element){angularD3plusUtils.controller($scope, $element,'stacked');}
 	  };
 	}])
+	.directive('d3plusTable',['angularD3plusUtils',function(angularD3plusUtils) {
+	  return {
+	  	restrict: 'AE',
+	  	scope: {
+            data: '=', 
+            id: '=',  
+            container: '@?',    
+            size: '@?'
+        },
+        template:  angularD3plusUtils.template,
+	  	link: angularD3plusUtils.link,
+	  	controller: function($scope, $element){angularD3plusUtils.controller($scope, $element,'table');}
+	  };
+	}])
+	.directive('d3plusTreemap',['angularD3plusUtils',function(angularD3plusUtils) {
+	  return {
+	  	restrict: 'AE',
+	  	scope: {
+            data: '=', 
+            id: '=',  
+            container: '@?',    
+            size: '@?'
+        },
+        template:  angularD3plusUtils.template,
+	  	link: angularD3plusUtils.link,
+	  	controller: function($scope, $element){angularD3plusUtils.controller($scope, $element,'treemap');}
+	  };
+	}])
+
+	
+
 	.factory('angularD3plusUtils', function(){
 		var services= {
 			myid: function($scope,$element){
 				var name;
 				if($scope.container) name = $scope.container;
-				else if($scope.length>0) name = $scope[0].container;
 				else if($element) name = $element.container;
 				else if($scope.viz) name = $scope.viz.container();
 				console.log(name);
 				return name;
 	  		},
 	  		template: function($scope){
+	  			var divid;
 	  			if(!$scope.container){
-	  				$scope.container="d3plusRadar-"+new Date().getTime()
+	  				divid='d3plus-'+new Date().getTime();
+	  				$scope.container='#'+divid;
 	  				if($scope.length>0)$scope[0].container=$scope.container;
 	  			}
-	        	return '<div id="'+services.myid($scope)+'"></div>'
+	        	return '<div id="'+divid+'"></div>';
 	        },
 		  	link: function($scope, $element, $attrs){
 		  		$scope.$watch('data', function(data){
@@ -71,7 +258,7 @@
 	            });
 		  	},
 		  	controller: function($scope, $element,type) {
-		  		$scope.viz=d3plus.viz().container("#"+services.myid($scope,$element));
+		  		$scope.viz=d3plus.viz().container(services.myid($scope,$element));
 		  		if($scope.id) $scope.viz.id($scope.id);
 		  		if($scope.size) $scope.viz.size($scope.size);
 		  		$scope.viz.type(type);
