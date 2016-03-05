0794969265d167cfbdc5ac3ddd4bfc0c6f514a64
Mario Mol <mariohmol@gmail.com>
2016-03-05 12:55:14 -0300

    first version running directives

--- src/angular-d3plus.js
+++ src/angular-d3plus.js
@@ -2,16 +2,48 @@
 	'use strict';
 
 	angular.module('angular-d3plus', [])
-	.directive('d3plus-treemap', function() {
+	.directive('d3plusRadar',['angularD3plusUtils',function(angularD3plusUtils) {
+		
+	  return {
+	  	restrict: 'E',
+	  	scope: {
+            data: '=',  
+            container: '=',    
+            id: '=',   
+            size: '=?'
+        },
+        template: '<div>AEEE</div>',
+	  	link: function(scope, element, attrs){
+	  		scope.$watch('data', function(data){
+                if (data) {
+                    if (scope.onReady && typeof scope.onReady() === 'function') scope.onReady()(scope, element);
+                }
+            });
+	  	},
+	  	controller: function($scope, $element) {
+	  	}
+	  };
+	}])
+	.directive('d3plusTreemap',['angularD3plusUtils',function(angularD3plusUtils) {
 	  return {
 	  	restrict: 'AE',
+	  	scope: {
+            data: '=',  
+            container: '=',    
+            id: '=',   
+            size: '=?'
+        },
 	  	link: function(scope, element, attrs){
+	  		console.log('oioio');
 	  	},
-	    templateUrl: function(elem, attr){
-	      return 'customer-'+attr.type+'.html';
-	    }
+	    template: '<div></div>'
 	  };
-	})
-	.factory('angular-d3plus-utils', function(){
+	}])
+	.factory('angularD3plusUtils', function(){
+		return {
+			log: function(){
+				console.log('log');
+			}
+		};
 	});
 })();
